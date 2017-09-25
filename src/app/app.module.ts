cc03cd5647739d1f5c1c4896eb21b7a5b6f41b78
Hai Nguyen <phihai91@Hai.local>
2017-09-25 15:36:37 +0700

    move folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { LoginComponent } from './core/components/login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
